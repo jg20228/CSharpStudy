52a517950f71ab83f6fd17bc563ae22ee3fa2102
home <jg20228@nate.com>
2020-10-10 23:28:57 +0900

    Hello C#

---
+++ CSharpStudy/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CSharpStudy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("안녕! C#");
+
+            Console.Write("Hello C#");
+        }
+    }
+}
