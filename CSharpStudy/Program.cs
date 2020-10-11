a61dce5c70d523966a8695ace20a8b45cdf9d15f
home <jg20228@nate.com>
2020-10-11 23:41:41 +0900

    Generic과 User클래스 담기

--- CSharpStudy/Program.cs
+++ CSharpStudy/Program.cs
@@ -7,28 +7,79 @@ namespace CSharpStudy
     {
         static void Main(string[] args)
         {
-            //var num = 1;
-            //for (; num <= 10; num++)
+            // 1 2 3 4 5 6 7 8 9 10
+
+            // var num = 1;
+
+            // Generic List 
+            //새로운 메모리에 적재될 공간이 마련되어야해서 new를 함
+            //List<string> list = new List<string>();
+            //list.Add("text1");
+            //list.Add("text2");
+            //list.Add("text3");
+            //list.Add("text4");
+            //list.Add("text5");
+
+            //foreach (var text in list)
             //{
-            //    Console.WriteLine(num);
+            //    Console.WriteLine(text);
             //}
 
-            List<int> numberList = new List<int>();
+            //유지보수할때 뒤에 제네릭만 수정하면 되어서 편함
+            //var list = new List<int>();
+            //list.Add(1);
 
-            numberList.Add(1);
-            numberList.Add(2);
-            numberList.Add(3);
-            numberList.Add(4);
-            numberList.Add(5);
 
-            foreach(var num in numberList)
+
+            var list = new List<string>()
             {
-                Console.WriteLine(num);
+                "text1",
+                "text2",
+                "text3",
+                "text4",
+                "text5"
+            };
+
+            foreach(var text in list)
+            {
+                Console.WriteLine(text);
             }
 
-            for(var index=1; index<=numberList.Count; index++)
+            //-------------------------------------
+            //1 2 3 4 5 6 7 8 9 10 x
+            //1 2 3 4 5 6 7 8 9 10 y
+            //2차원 배열로 표현하는게 DB
+
+            //ex
+            //번호  이름     나이 연락처
+            // 01   임꺽정   20  010-0000-0000
+            // 02   홍길동   20  010-0000-0000
+
+            var user1 = new User()
             {
-                Console.WriteLine(numberList[index]);
+                No = 1,
+                Name = "임꺽정",
+                Age = 30,
+                Phone = "010-0000-0000"
+            };
+
+            var user2 = new User()
+            {
+                No=2,
+                Name="홍길동",
+                Age = 30,
+                Phone = "010-0000-0000"
+            };
+
+            var userList = new List<User>()
+            {
+                user1,user2
+            };
+
+            foreach(var user in userList)
+            {
+                Console.WriteLine("번호 : " + user.No);
+                Console.WriteLine("이름 : " + user.Name);
             }
         }
     }
---
+++ CSharpStudy/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+
+namespace CSharpStudy
+{
+    class User
+    {
+        // JAVA에서 getter와 setter 사용할때
+        //private int id;
+        //private string name;
+
+        //public void setId(int id)
+        //{
+        //    this.id = id;
+        //}
+        //public int getId()
+        //{
+        //    return id;
+        //}
+
+        //--------------------------
+        // C# 3.0이상에서 가능 
+        // 번호, 이름, 나이, 연락처
+
+        public int No { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Phone { get; set; }
+    }
+}
